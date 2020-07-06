81fc90da42a29c46d03fc31dce88f8bd791314dd
Leigh <leighdaubermann@me.com>
2020-07-06 17:31:37 +0200

    st

---
+++ build/precache-manifest.7f78022b19016054512ee57f512caf12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096d17890136fb72a5097eced22e10db",
+    "url": "https://master.d2m37dimhoingy.amplifyapp.com/index.html"
+  },
+  {
+    "revision": "d8aa21161834ce68c01e",
+    "url": "https://master.d2m37dimhoingy.amplifyapp.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97b02b3600015bc568d4",
+    "url": "https://master.d2m37dimhoingy.amplifyapp.com/static/js/2.1a0694e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://master.d2m37dimhoingy.amplifyapp.com/static/js/2.1a0694e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8aa21161834ce68c01e",
+    "url": "https://master.d2m37dimhoingy.amplifyapp.com/static/js/main.b712cbcc.chunk.js"
+  },
+  {
+    "revision": "7640864d635d80e7c735",
+    "url": "https://master.d2m37dimhoingy.amplifyapp.com/static/js/runtime-main.809df57e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://master.d2m37dimhoingy.amplifyapp.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
