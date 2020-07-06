2f2448fe9782a97b143632e3452a1910c56f4df5
Leigh <leighdaubermann@me.com>
2020-07-06 17:33:07 +0200

    lkjhweq

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now manifest update
+          Developed by Leigh
         </a>
       </header>
     </div>
